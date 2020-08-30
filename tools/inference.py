3db883c0e23b8d1d2b3b74091d3d1536156972c4
alexchung <yonganzhong@outlook.com>
2020-08-30 16:38:19 +0800

    add inference module

---
+++ tools/inference.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : inference.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/8/30 下午4:37
+# @ Software   : PyCharm
+#-------------------------------------------------------
